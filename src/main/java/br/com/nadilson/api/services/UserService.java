d6afc4c7eab2c1776c8837eb3652bfdca0b1daec
Nadilson <nadilson@protonmail.com>
2024-07-17 05:21:51 -0300

    Endpoint findAll() para User

--- src/main/java/br/com/nadilson/api/services/UserService.java
+++ src/main/java/br/com/nadilson/api/services/UserService.java
@@ -2,8 +2,12 @@ package br.com.nadilson.api.services;
 
 import br.com.nadilson.api.domain.User;
 
+import java.util.List;
+
 public interface UserService {
 
     User findById(Integer id);
+
+    List<User> findAll();
 
 }
