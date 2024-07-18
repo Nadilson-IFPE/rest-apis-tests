4b3e8e64a1066bfeaff96052df8d6c869c4f3556
Nadilson <nadilson@protonmail.com>
2024-07-18 07:35:06 -0300

    Teste para o método delete() da classe UserService

--- src/test/java/br/com/nadilson/api/services/impl/UserServiceImplTest.java
+++ src/test/java/br/com/nadilson/api/services/impl/UserServiceImplTest.java
@@ -157,6 +157,11 @@ class UserServiceImplTest {
     }
 
     @Test
-    void delete() {
+    void deleteWithSuccess() {
+        when(repository.findById(anyInt())).thenReturn(optionalUser);
+        doNothing().when(repository).deleteById(anyInt());
+        service.delete(ID);
+        verify(repository, times(1)).deleteById(anyInt());
     }
+
 }
